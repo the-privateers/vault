ad07876062dd0de0e5f774aae78f6dae642cc09c
Phil Stephens <phil.stephens.bne@gmail.com>
2016-11-29 15:02:09 +1000

    add link to navigate back to vault list

--- resources/views/vaults/vault/partials/toolbar.blade.php
+++ resources/views/vaults/vault/partials/toolbar.blade.php
@@ -5,7 +5,13 @@
 
 <div class="page-header clearfix" style="margin-top: 0;">
     <div class="row">
-        <div class="col-sm-4 col-sm-offset-8">
+        <div class="col-sm-4">
+            @if (Request::route()->getName() != 'vault.index')
+            <a href="{{ route('vault.index') }}" class="btn btn-default btn-block">Back to Vaults</a>
+            @endif
+        </div>
+
+        <div class="col-sm-4 col-sm-offset-4">
             <a href="{{ route('vault.create') }}" class="btn btn-default btn-block">Create Vault</a>
         </div>
     </div>
